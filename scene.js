9a987b685e99e136559a4135cf6c41228b7f42bd
robin <s1102585@student.windesheim.nl>
2019-02-13 17:49:02 +0100

    huisje

--- scene.js
+++ scene.js
@@ -67,6 +67,8 @@ function init() {
     ////////////
     //  cube  //
     ////////////
+
+    
     var material = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('images/crate.jpg') });
     var geometry = new THREE.CubeGeometry(50, 50, 50);
     var cube = new THREE.Mesh(geometry, material);
@@ -76,9 +78,29 @@ function init() {
     var geometry2 = new THREE.ConeGeometry( 60, 40, 4);
     var material2 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
     var cone = new THREE.Mesh( geometry2, material2 );
-    cone.position.set(0, 75, 0);
+    cone.position.set(0, 70, 0);
     cone.rotateY(Math.PI / 4);
     scene.add( cone );
+
+    var material3 = new THREE.MeshBasicMaterial({color: 0xffff00});
+    var geometry3 = new THREE.CubeGeometry(10, 20, 1);
+    var cube2 = new THREE.Mesh(geometry3, material3);
+    cube2.position.set(12, 12.5, 25);
+    scene.add(cube2);
+
+    var materialwindow = new THREE.MeshBasicMaterial({color: 0xffff00});
+    var geometrywindow = new THREE.CubeGeometry(10, 10, 1);
+    var cube3 = new THREE.Mesh(geometrywindow, materialwindow);
+    cube3.position.set(12, 35, 25);
+    scene.add(cube3);
+
+    var cube4 = new THREE.Mesh(geometrywindow, materialwindow);
+    cube4.position.set(-12, 35, 25);
+    scene.add(cube4);
+
+    var window3 = new THREE.Mesh(geometrywindow,materialwindow);
+    window3.position.set(-12, 12.5, 25);
+    scene.add(window3);
 
 }
 
