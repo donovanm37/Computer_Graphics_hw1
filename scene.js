dc38c8c4f79ea8700dca9b7d84953a6d1d04ed34
robin <s1102585@student.windesheim.nl>
2019-02-16 19:39:17 +0100

    comments

--- scene.js
+++ scene.js
@@ -1,69 +1,52 @@
 var scene, camera, renderer;
-var loadedObject = null;
+var loadedObject = null;    // In this variable will be a loadedobject saved so it can be used to animate with the object.
 var loadedObject2 = null;
 var loadedObject3 = null;
-var spotLight, lightHelper, shadowCameraHelper;
 init();
 animate();
 
 function init() {
-    ////////////
-    // scene  //
-    ////////////
+    //Scene
     scene = new THREE.Scene();
-
-    ////////////
-    // camera //
-    ////////////
+    //Camera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 20000);
-    camera.position.set(0, 100, 200);
+    camera.position.set(0, 100, 200); // set camera position
     camera.lookAt(scene.position);
 
     /// camera movement by keypress
-    document.addEventListener( 'keypress', onDocumentKeyPress, false );
+    document.addEventListener( 'keypress', onDocumentKeyPress, false ); // to move camera with WASD
 
-
-
-    //////////////
-    // renderer //
-    //////////////
+    //Renderer
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    ////////////
-    //  axes  //
-    ////////////
-    var axes = new THREE.AxisHelper(100);
-    scene.add(axes);
 
-    ////////////
-    // floor  //
-    ////////////
-    var floorTexture = new THREE.ImageUtils.loadTexture('images/Grass.jpg');
+    // Lets make the ground
+    var floorTexture = new THREE.ImageUtils.loadTexture('images/Grass.jpg'); // Use grass texture
     floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
-    floorTexture.repeat.set(10, 10);
+    floorTexture.repeat.set(10, 10); // make the picture repeat so its not one blurry picture
     var floorMaterial = new THREE.MeshBasicMaterial({ map: floorTexture, side: THREE.DoubleSide });
-    var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
-    var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    floor.position.y = -0.5;
-    floor.rotation.x = Math.PI / 2;
-    scene.add(floor);
+    var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10); // geometry of the ground
+    var floor = new THREE.Mesh(floorGeometry, floorMaterial); //make the mesh of the grass
+    floor.position.y = -0.5; // put it a bit under the middle of the screen
+    floor.rotation.x = Math.PI / 2; // make it look like a ground and not a wall
+    scene.add(floor); // add to teh scene
 
-    var floorTexture2 = new THREE.ImageUtils.loadTexture('images/asphalt.jpg');
-    floorTexture2.wrapS = floorTexture2.wrapT = THREE.RepeatWrapping;1
-    floorTexture2.repeat.set(10, 10);
-    var floorMaterial2 = new THREE.MeshBasicMaterial({ map: floorTexture2, side: THREE.DoubleSide });
-    var floorGeometry2 = new THREE.PlaneGeometry(1000, 200, 10, 10);
-    var floor2 = new THREE.Mesh(floorGeometry2, floorMaterial2);
-    floor2.position.set(0,0,140);
-    floor2.rotation.x = Math.PI / 2;
-    scene.add(floor2);
+    //lets make the road
+    var RoadTexture = new THREE.ImageUtils.loadTexture('images/asphalt.jpg'); // use asphalt texture
+    RoadTexture.wrapS = RoadTexture.wrapT = THREE.RepeatWrapping;
+    RoadTexture.repeat.set(10, 10); // make the texture repeat so it wont be one blurry picture
+    var RoadMaterial = new THREE.MeshBasicMaterial({ map: RoadTexture, side: THREE.DoubleSide });
+    var RoadGeometry = new THREE.PlaneGeometry(1000, 200, 10, 10); // make the road of a plane
+    var Road = new THREE.Mesh(RoadGeometry, RoadMaterial); // make the Mesh
+    Road.position.set(0,0,140); // set at correct position
+    Road.rotation.x = Math.PI / 2; // make it a road and not a wall
+    scene.add(Road); // add to scene
 
-    ////////////
-    // skybox //
-    ////////////
-    var materialArray = [];
+
+    //Skybox
+    var materialArray = []; // put all the png in an array
     materialArray.push(new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('images/dawnmountain-xpos.png') }));
     materialArray.push(new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('images/dawnmountain-xneg.png') }));
     materialArray.push(new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('images/dawnmountain-ypos.png') }));
@@ -71,13 +54,16 @@ function init() {
     materialArray.push(new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('images/dawnmountain-zpos.png') }));
     materialArray.push(new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('images/dawnmountain-zneg.png') }));
     for (var i = 0; i < 6; i++) {
-        materialArray[i].side = THREE.BackSide;
+        materialArray[i].side = THREE.BackSide; // get the backside
     }
-    var skyboxMaterial = new THREE.MeshFaceMaterial(materialArray);
-    var skyboxGeom = new THREE.CubeGeometry(5000, 5000, 5000, 1, 1, 1);
-    var skybox = new THREE.Mesh(skyboxGeom, skyboxMaterial);
-    scene.add(skybox);
-    
+    var skyboxMaterial = new THREE.MeshFaceMaterial(materialArray); // put it in a material
+    var skyboxGeom = new THREE.CubeGeometry(5000, 5000, 5000, 1, 1, 1); // make the geometry
+    var skybox = new THREE.Mesh(skyboxGeom, skyboxMaterial); // make the skybox
+    scene.add(skybox); // add the skybox to the scene
+
+
+    //Use the functions from FunctionsBuilds.js to make the street
+
     House(400,25,-50, false);
     House(300,25,-50, false);
     House(200,25,-50, false);
@@ -148,7 +134,31 @@ function init() {
     Plane(0,300,-100);
     Car(-70,0,100);
     Cloud(-70,300,100);
-    MailBox(30, 14, 0);
+
+    MailBox(435, 14, 0);
+    MailBox(335, 14, 0);
+    MailBox(235, 14, 0);
+    MailBox(135, 14, 0);
+    MailBox(35, 14, 0);
+    MailBox(-65, 14, 0);
+    MailBox(-165, 14, 0);
+    MailBox(-265, 14, 0);
+    MailBox(-365, 14, 0);
+    MailBox(-465, 14, 0);
+
+
+    MailBox(405, 14, 270, true);
+    MailBox(305, 14, 270, true);
+    MailBox(205, 14, 270, true);
+    MailBox(105, 14, 270, true);
+    MailBox(5, 14, 270, true);
+    MailBox(-95, 14, 270, true);
+    MailBox(-195, 14, 270, true);
+    MailBox(-295, 14, 270, true);
+    MailBox(-395, 14, 270, true);
+    MailBox(-495, 14, 270, true);
+
+    //add OrbitControls
     var controls = new THREE.OrbitControls(camera);
     controls.autoRotate = true;
     controls.autoRotateSpeed = 2;
@@ -156,6 +166,7 @@ function init() {
 
 }
 
+//Animations:
 function animate() {
     requestAnimationFrame(animate);
     MoveCar();
@@ -164,61 +175,53 @@ function animate() {
     render();
 }
 
+//moving car animation
 function MoveCar() {
-    loadedObject.position.x += 2;
-    if (loadedObject.position.x === 450) {
+    loadedObject.position.x += 2; // make it move by adding 2
+    if (loadedObject.position.x === 450) { // when it reaches end of road reset to beginning
         loadedObject.position.set(-450,0,100);
     }
 }
 
 function MovePlane() {
-    loadedObject3.rotateX(Math.PI / 180);
-    loadedObject3.translateZ(-1);
+    loadedObject3.rotateX(Math.PI / 180); // make the plane rotate arround itself
+    loadedObject3.translateZ(-1); // move a bit forward so it looks like its doing a loop
 
 }
 
-function MoveCloud() {
-    loadedObject2.position.x += 1;
-    if (loadedObject2.position.x === 450) {
+function MoveCloud() { // move the clouds
+    loadedObject2.position.x += 1; // make the clouds move a bit
+    if (loadedObject2.position.x === 450) { // when the clouds reach the end spawn at random position and move again
         loadedObject2.position.set(-450,Math.floor(Math.random() * 101) + 250  ,Math.floor(Math.random() * 1000) - 500  );
     }
 }
 function update() {
 
 }
-
+    //render
 function render() {
     renderer.render(scene, camera);
 }
-
+ // make camera move by WASD
 function onDocumentKeyPress( event ) {
 
-    var keyCode = event.which;
+    var keyCode = event.which; // get which key is pressed
     var positionDelta = 20;
-    //A
-    if ( keyCode == 97 )
+    if ( keyCode === 97 ) // A
     {
-
         camera.position.x -= positionDelta;
     }
-    //D
-    else if ( keyCode == 100 )
+    else if ( keyCode === 100 ) // D
     {
-
         camera.position.x += positionDelta;
     }
-    //W
-    else if ( keyCode == 119 )
+    else if ( keyCode === 119 ) // W
     {
         camera.position.z -= positionDelta;
     }
-    //S
-    else if ( keyCode == 115 )
+    else if ( keyCode === 115 ) // S
     {
         camera.position.z += positionDelta;
-
     }
     camera.updateProjectionMatrix();
 }
-
-var clock = new THREE.Clock();
--- three/FunctionsBuilds.js
+++ three/FunctionsBuilds.js
@@ -1,10 +1,13 @@
-function House(x, y, z, rotate) {
+function House(x, y, z, rotate) { // To make a house given position where to place it, and rotate = true if on other side of street
+
+    //Make the base of the house
     var material = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('images/red_bricks.jpg') });
     var geometry = new THREE.CubeGeometry(50, 50, 50);
     var cube = new THREE.Mesh(geometry, material);
     cube.position.set(x, y, z);
     scene.add(cube);
 
+    //Add a roof
     var geometry2 = new THREE.ConeGeometry( 50, 30, 4);
     var material2 = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('images/roofTexture.jpg')});
     var roof = new THREE.Mesh( geometry2, material2 );
@@ -12,6 +15,7 @@ function House(x, y, z, rotate) {
     roof.rotateY(Math.PI / 4);
     scene.add(roof);
 
+    //Add a door
     var material3 = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('images/doorTexture.jpg')});
     var geometry3 = new THREE.CubeGeometry(10, 20, 1);
     var door = new THREE.Mesh(geometry3, material3);
@@ -21,6 +25,7 @@ function House(x, y, z, rotate) {
     }
     scene.add(door);
 
+    //add window1
     var materialwindow = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('images/windowTexture.jpg')});
     var geometrywindow = new THREE.CubeGeometry(10, 10, 1);
     var window1 = new THREE.Mesh(geometrywindow, materialwindow);
@@ -30,6 +35,7 @@ function House(x, y, z, rotate) {
     }
     scene.add(window1);
 
+    //add window 2
     var window2 = new THREE.Mesh(geometrywindow, materialwindow);
     window2.position.set(x - 12, y + 10, z + 25);
     if (rotate === true) {
@@ -37,6 +43,8 @@ function House(x, y, z, rotate) {
     }
     scene.add(window2);
 
+
+    //add window3
     var window3 = new THREE.Mesh(geometrywindow,materialwindow);
     window3.position.set(x - 12, y - 12.5, z + 25);
     if (rotate === true) {
@@ -44,6 +52,7 @@ function House(x, y, z, rotate) {
     }
     scene.add(window3);
 
+    //add a chimney
     var geometrychimney = new THREE.CubeGeometry(10, 25, 10);
     var chimney = new THREE.Mesh(geometrychimney, material2);
     chimney.position.set(x + 12.5, y + 50, z);
@@ -52,35 +61,43 @@ function House(x, y, z, rotate) {
 
 }
 
+//Make a tree
 function Tree(x,y,z) {
+    //add the trunk of the tree
     var geometry = new THREE.CylinderGeometry( 5, 5, 50, 64 );
     var material = new THREE.MeshBasicMaterial( {color: 0x8b4513} );
     var cylinder = new THREE.Mesh( geometry, material );
     cylinder.position.set(x,y,z);
     scene.add(cylinder);
 
+    //add the spheres to make it look like a tree
     var geometry2 = new THREE.SphereGeometry( 10, 32, 32 );
     var material2 = new THREE.MeshLambertMaterial( {color: 0x136207, reflectivity: 30} );
     var sphere = new THREE.Mesh( geometry2, material2 );
     sphere.position.set(x,y + 25,z);
     scene.add(sphere);
 
+    //add another sphere
     var geometry3 = new THREE.SphereGeometry( 14, 32, 32 );
     var sphere2 = new THREE.Mesh( geometry3, material2 );
     sphere2.position.set(x,y + 10,z);
     scene.add(sphere2);
 
+    //and another sphere
     var geometry4 = new THREE.SphereGeometry( 16, 32, 32 );
     var sphere3 = new THREE.Mesh( geometry4, material2 );
     sphere3.position.set(x,y,z);
     scene.add(sphere3);
 }
 
+//we cane make a sun
 function Sun(x, y, z) {
+    //a new directional lightning to make the sun give light
     var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
     directionalLight.position.set(x,y,z);
-    scene.add( directionalLight );
+    scene.add( directionalLight ); // point it towards the middle
 
+    //make a sphere so it looks like there is a sun
     var geometry = new THREE.SphereGeometry( 20, 32, 32 );
     var material = new THREE.MeshBasicMaterial( {color: 0xf9d71c} );
     var sphere = new THREE.Mesh( geometry, material );
@@ -89,49 +106,54 @@ function Sun(x, y, z) {
 
 }
 
+// make a street lantern
 function Lantern(x,y,z) {
+    //make the pole of the lantern
     var geometry = new THREE.CylinderGeometry( 5, 5, 50, 64 );
     var material = new THREE.MeshPhongMaterial( {color: 0x808080} );
     var cylinder = new THREE.Mesh( geometry, material );
     cylinder.position.set(x,y,z);
     scene.add(cylinder);
 
+    // make the lamp of the lantern
     var geometry3 = new THREE.SphereGeometry( 7, 32, 32, 0, 6.3, 2,1.3);
     var material3 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
     var lamp = new THREE.Mesh( geometry3, material3 );
     lamp.position.set(x,y + 25,z);
     scene.add(lamp);
 
+    //make the top of the lantern
     var geometry2 = new THREE.CylinderBufferGeometry( 10, 5, 10, 8 );
-    var material2 = new THREE.MeshPhongMaterial( {color: 0xffffff, shiny: 20} );
+    var material2 = new THREE.MeshPhongMaterial( {color: 0xffffff, shininess: 20} );
     var cylinder2 = new THREE.Mesh( geometry2, material2 );
     cylinder2.position.set(x,y + 25,z);
     scene.add( cylinder2 );
 
 }
 
+// make a car with the use of the mtl and obj loader
 function Car(x,y,z){
-    var mtlLoader = new THREE.MTLLoader();
-    mtlLoader.setPath( 'Objects/Materials/' );
-    url = "Lamborghini_Aventador.mtl";
-    mtlLoader.load( url, function( materials ) {
+    var mtlLoader = new THREE.MTLLoader(); // get mtlloader
+    mtlLoader.setPath( 'Objects/Materials/' ); // set the path of the mtl file
+    url = "Lamborghini_Aventador.mtl"; //set the mtl file
+    mtlLoader.load( url, function( materials ) { // load the file
 
-        materials.preload();
+        materials.preload(); //set the materials
 
-        var objLoader = new THREE.OBJLoader();
-        objLoader.setMaterials( materials );
-        objLoader.setPath( 'Objects/' );
-        objLoader.load( 'Lamborghini_Aventador.obj', function ( object ) {
-                object.position.set(x,y,z);
-                object.rotateY(Math.PI / 2);
-                object.scale.set(0.2,0.2,0.2);
-                loadedObject = object;
-                scene.add( object );
+        var objLoader = new THREE.OBJLoader(); // get objloader
+        objLoader.setMaterials( materials ); //set the materials of the object
+        objLoader.setPath( 'Objects/' ); // set path of the obj file
+        objLoader.load( 'Lamborghini_Aventador.obj', function ( object ) { //load the obj file
+                object.position.set(x,y,z); //set pos
+                object.rotateY(Math.PI / 2); // make it point correctly
+                object.scale.set(0.2,0.2,0.2); // make it smaller cuz obj files are giant
+                loadedObject = object; // set it in the variable so it can be animated
+                scene.add( object ); //add to scene
 
             },
             // called when loading is in progresses
             function ( xhr ) {
-
+                //in console log make it so you can see how fast its done
                 console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
             },
@@ -144,6 +166,7 @@ function Car(x,y,z){
 
     });
 }
+
 
 function Cloud(x,y,z) {
     var loader = new THREE.OBJLoader();
@@ -208,7 +231,7 @@ function Plane(x,y,z) {
     });
 }
 
-function MailBox(x,y,z){
+function MailBox(x,y,z,rotate){
     var mtlLoader = new THREE.MTLLoader();
     mtlLoader.setPath( 'Objects/Materials/' );
     var url = "pstblpll2.mtl";
@@ -222,6 +245,9 @@ function MailBox(x,y,z){
         objLoader.load( 'pstblpll2.obj', function ( object ) {
                 object.position.set(x,y,z);
                 object.scale.set(3,3,3);
+                if (rotate === true) {
+                    object.rotateY(Math.PI);
+                }
                 scene.add( object );
 
             },
